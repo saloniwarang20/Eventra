216fe6a1db89fdcf8a97b886849a0066c6643978
saloniwarang20 <saloniwarang20@gmail.com>
2026-02-20 10:54:14 +0530

    Fix production cookie config

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -69,8 +69,8 @@ export const register = async (req, res)=>{
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite:'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -118,8 +118,8 @@ export const login =  async (req, res)=>{
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite:'none' ,
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -134,8 +134,8 @@ export const logout = async (req,res)=>{
     try {
         res.clearCookie('token',{
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite:'none',
         })
 
         return res.json({success: true, message: "Logged Out"})
